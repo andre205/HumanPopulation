94c701b925c9b1584da439b3de2603b026add85b
andre205 <andre205@mail.chapman.edu>
2018-04-04 20:04:34 -0700

    we got plots

--- gui.py
+++ gui.py
@@ -18,6 +18,10 @@ import numpy as np
 import os, sys, time
 from shutil import copyfile
 
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+
 import threading
 from threading import Thread
 
@@ -25,7 +29,7 @@ class Window(QMainWindow):
     resized = pyqtSignal()
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(100, 100, 620, 500)
+        self.setGeometry(100, 100, 900, 500)
         self.setWindowTitle('Population Simulator 1.0')
         self.init_ui()
 
@@ -35,64 +39,53 @@ class Window(QMainWindow):
         QApplication.setStyle(QStyleFactory.create('Fusion'))
         self.pop_img = QLabel(self)
         self.update_pop_img()
-        self.pop_img.move(100,20)
+        self.pop_img.move(20,10)
 
         self.tbx1 = QLineEdit(self)
-        self.tbx1.move(100, 300)
         self.tbx1.resize(100,30)
-        self.tbx1.setText("test")
+        self.tbx1.move(750, 400)
+        self.tbx1.setText("5")
 
         self.x_label = QLabel('LABEL', self)
         self.x_label.setFont(boldFont)
         self.x_label.resize(150, 10)
-        self.x_label.move(20, 310)
+        self.x_label.move(700, 400)
 
         self.start_button = QPushButton('START', self)
-        self.start_button.move(100, 450)
+        self.start_button.move(650, 450)
         self.start_button.clicked.connect(self.start)
 
         self.reset_button = QPushButton('STOP', self)
-        self.reset_button.move(250, 450)
+        self.reset_button.move(775, 450)
         self.reset_button.clicked.connect(self.reset)
 
         self.show()
 
 
     def start(self, event):
-        x=1
-        # tbx1 = self.tbx1.text()
-        # tby1 = self.tby1.text()
-        # tbx2 = self.tbx2.text()
-        # tby2 = self.tby2.text()
-        #
-        # tbr = self.rgb_r.text()
-        # tbg = self.rgb_g.text()
-        # tbb = self.rgb_b.text()
-        #
-        # tbpx = self.px_ct.text()
-        #
-        # REGION = (int(tbx1), int(tby1), int(tbx2), int(tby2))
-        # RGB = [int(tbr), int(tbg), int(tbb)]
-        #
-        # th = Thread(target = monitor(REGION, RGB, int(tbpx)))
-        # th.start()
-        # th.join()
-        # #monitor(REGION, RGB)
+        y0 = float(self.tbx1.text())
+        t = np.linspace(0,20)
+        y = odeint(model,y0,t)
+        plt.plot(t,y)
+        plt.xlabel('time')
+        plt.ylabel('y(t)')
+        #plt.show()
+        plt.savefig('out.png')
+        update_image(self)
 
     def resizeEvent(self, event):
         self.resized.emit()
         return super(Window, self).resizeEvent(event)
 
+    def reset(self, event):
+        plt.clf()
+        self.update_pop_img()
 
     def update_pop_img(self):
         image_filepath = 'out_img.png'
         self.pixmap = QPixmap(image_filepath)
         self.pop_img.setPixmap(self.pixmap)
-        self.pop_img.resize(400,200)
-
-    def reset(self, event):
-        x=1
-
+        self.pop_img.resize(600,500)
 
 def run():
     app = QApplication(sys.argv)
@@ -100,13 +93,20 @@ def run():
     sys.exit(app.exec_())
 
 
-# def update_image(REGION):
-#     screen =  ImageGrab.grab(bbox=REGION)
-#     img_np = np.array(screen.getdata(),dtype='uint8')\
-#     .reshape((screen.size[1],screen.size[0],3))
-#     img_np = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-#     cv2.imwrite('out_img.png', img_np)
-#     cv2.destroyAllWindows()
+def update_image(self):
+    image_filepath = 'out.png'
+    self.pixmap = QPixmap(image_filepath)
+    #self.pixmap = self.pixmap.scaled(450, 450)
+    self.pop_img.setPixmap(self.pixmap)
+    # self.pop_img.scaledToWidth(200)
+
+# function that returns dy/dt
+def model(y,t):
+    k = 0.3
+    dydt = -k * y
+    return dydt
+
+
 
 if __name__ == '__main__':
     run()
