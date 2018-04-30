9617879f60c45c4cf801661f1f1805bef7341d32
andre205 <andre205@mail.chapman.edu>
2018-04-30 00:35:56 -0700

    update

--- gui2.py
+++ gui2.py
@@ -29,7 +29,7 @@ class Window(QMainWindow):
     resized = pyqtSignal()
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(100, 100, 900, 500)
+        self.setGeometry(800, 300, 900, 500)
         self.setWindowTitle('Population Simulator 1.0')
         self.init_ui()
 
@@ -41,20 +41,70 @@ class Window(QMainWindow):
         self.update_pop_img()
         self.pop_img.move(20,10)
 
-        self.tbx1 = QLineEdit(self)
-        self.tbx1.resize(100,30)
-        self.tbx1.move(750, 400)
-        self.tbx1.setText("5")
-
         self.top_label = QLabel('EQUATION', self)
         self.top_label.setFont(boldFont)
         self.top_label.resize(150, 10)
         self.top_label.move(700, 50)
 
-        self.eq_label = QLabel('dy/dt = k * (1-(y/1000)) * y', self)
+        self.eq_label = QLabel('dx/dt = ax - pxy', self)
         self.eq_label.setFont(boldFont)
         self.eq_label.resize(200, 30)
         self.eq_label.move(700, 70)
+
+        self.eq_label2 = QLabel('dy/dt = -by + qxy', self)
+        self.eq_label2.setFont(boldFont)
+        self.eq_label2.resize(200, 30)
+        self.eq_label2.move(700, 90)
+
+        self.tbx_a = QLineEdit(self)
+        self.tbx_a.resize(100,30)
+        self.tbx_a.move(750, 120)
+        self.tbx_a.setText("2")
+
+        self.tbx_p = QLineEdit(self)
+        self.tbx_p.resize(100,30)
+        self.tbx_p.move(750, 160)
+        self.tbx_p.setText("3")
+
+        self.tbx_b = QLineEdit(self)
+        self.tbx_b.resize(100,30)
+        self.tbx_b.move(750, 200)
+        self.tbx_b.setText("5")
+
+        self.tbx_q = QLineEdit(self)
+        self.tbx_q.resize(100,30)
+        self.tbx_q.move(750, 240)
+        self.tbx_q.setText("5")
+
+        self.tbx_y = QLineEdit(self)
+        self.tbx_y.resize(100,30)
+        self.tbx_y.move(750, 400)
+        self.tbx_y.setText("5")
+
+        self.label_a = QLabel('a:', self)
+        self.label_a.setFont(boldFont)
+        self.label_a.resize(200, 30)
+        self.label_a.move(700, 120)
+
+        self.label_p = QLabel('p:', self)
+        self.label_p.setFont(boldFont)
+        self.label_p.resize(200, 30)
+        self.label_p.move(700, 160)
+
+        self.label_b = QLabel('b:', self)
+        self.label_b.setFont(boldFont)
+        self.label_b.resize(200, 30)
+        self.label_b.move(700, 200)
+
+        self.label_q = QLabel('q:', self)
+        self.label_q.setFont(boldFont)
+        self.label_q.resize(200, 30)
+        self.label_q.move(700, 240)
+
+        self.label_y = QLabel('y:', self)
+        self.label_y.setFont(boldFont)
+        self.label_y.resize(200, 30)
+        self.label_y.move(700, 400)
 
 
         self.start_button = QPushButton('START', self)
@@ -113,9 +163,8 @@ def update_image(self):
     # self.pop_img.scaledToWidth(200)
 
 # function that returns dy/dt
-def model(y,t):
-    k = 1
-    dydt = k * (1-(y/1000)) * y
+def model(a,b,p,q,y,x,t):
+    dydt =
     return dydt
 
 if __name__ == '__main__':
