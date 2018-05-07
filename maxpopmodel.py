bbb67d5fa1bb2d791e24e3b907a854c6712f2d54
andre205 <andre205@mail.chapman.edu>
2018-05-06 23:10:03 -0700

    final touches

--- maxpopmodel.py
+++ maxpopmodel.py
@@ -2,11 +2,14 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+#2018 growth rate: 1.09% per year
+#Proposed carrying capacity: 10 billion
 def model(y,t):
     k = 1.09
     dydt = k * (1-(y/10)) * y
     return dydt
 
+#2018 population: 7.6 billion
 y0 = 7.632819325
 t = np.linspace(2018,2040)
 y = odeint(model,y0,t)
@@ -14,5 +17,4 @@ y = odeint(model,y0,t)
 plt.plot(t,y)
 plt.xlabel('time')
 plt.ylabel('human population (billion)')
-
-plt.savefig('test.png')
+plt.savefig('human_population_model.png')
