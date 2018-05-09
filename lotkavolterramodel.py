8f84db6ee20fe6c416a55919df4b7b27158d0e64
Tyler Andrews <andre205@mail.chapman.edu>
2018-05-09 15:12:02 -0700

    comments and cleanup

--- lotkavolterramodel.py
+++ lotkavolterramodel.py
@@ -3,40 +3,48 @@ import pylab as pl
 from scipy import integrate
 import vals
 
-#X is the form [X,Y]
+#XY is the form [X,Y]
 #dX_dt returns [ax-pxy, -by+qxy]
-def dX_dt(X, t=0):
-    return array([ vals.a*X[0] - vals.p*X[0]*X[1] , -vals.b*X[1] + vals.q*X[0]*X[1] ])
+def dX_dt(XY, t=0):
+    return array([ vals.a*XY[0] - vals.p*XY[0]*XY[1] , -vals.b*XY[1] + vals.q*XY[0]*XY[1] ])
 #jacobian
-def Jac(X, t=0):
-    return array([ [vals.a - vals.p * X[1], -vals.p*X[0] ], [ vals.q*X[1] , -vals.b +vals.q*X[0] ] ])
+def Jac(XY, t=0):
+    return array([ [vals.a - vals.p * XY[1], -vals.p*XY[0] ], [ vals.q*XY[1] , -vals.b +vals.q*XY[0] ] ])
 
 def lv_plot(a,p,b,q,X,Y,t0,tf,tdi):
-
+    #update user values
     vals.a = a
     vals.p = p
     vals.b = b
     vals.q = q
 
+    #critical points
     cp1 = array([0. , 0.])
     cp2 = array([b/q, a/p])
 
-    cp1_sol = Jac(cp1) #these are just 0
+    #evaluate critical points
+    cp1_sol = Jac(cp1)
     cp2_sol = Jac(cp2)
 
-    #EIGENVALS  +/- i*sqrt(b*a)
+    #find eigenvals  ( +/- i*sqrt(ab) )
     lambda1, lambda2 = linalg.eigvals(cp2_sol)
-    #PERIOD
+
+    #period (later returned)
     pd = 2*pi/abs(lambda1)
 
-    #TIME
+    #time series
     t = linspace(t0, tf, tdi)
-    #INITIAL CONDITIONS
+
+    #initial conditions
     X0 = array([X, Y])
+
+    #integration
     X = integrate.odeint(dX_dt, X0, t)
 
+    #tuples transposed for x and y populations
     pop_x, pop_y = X.T
 
+    #plotting
     f1 = pl.figure()
     pl.plot(t, pop_x, 'r-', label='X')
     pl.plot(t, pop_y, 'b-', label='Y')
@@ -49,11 +57,15 @@ def lv_plot(a,p,b,q,X,Y,t0,tf,tdi):
 
     f1.savefig('img/lvimg.png')
 
+    #phase plane plotting
+
+    #line thickness and colors
     v = linspace(0.25, 1, 5)
     vc = pl.cm.binary(linspace(0.25, 1, len(v)))
 
     f2 = pl.figure()
 
+    #plot x vs y, no time axis
     for v, col in zip(v, vc):
         X0 = v * cp2
         X = integrate.odeint(dX_dt, X0, t)
@@ -62,13 +74,19 @@ def lv_plot(a,p,b,q,X,Y,t0,tf,tdi):
     #find max values
     ymax = pl.ylim(ymin=0)[1]
     xmax = pl.xlim(xmin=0)[1]
+
     #mesh grid density
     nb_points = 20
+
+    #define grid
     x = linspace(0, xmax, nb_points)
     y = linspace(0, ymax, nb_points)
-
     X1 , Y1  = meshgrid(x, y)
+
+    #compute derivatives
     DX1, DY1 = dX_dt([X1, Y1])
+
+    #insure there is no division by 0
     M = (hypot(DX1, DY1))
     M[M == 0] = 1.
     DX1 /= M
@@ -84,4 +102,5 @@ def lv_plot(a,p,b,q,X,Y,t0,tf,tdi):
     pl.ylim(0, ymax)
     f2.savefig('img/ppimg.png')
 
+    #return the period for display on model page
     return pd
